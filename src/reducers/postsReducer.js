d52ff727a38dc3bb4e80af61424a89882cbbc7fb
KJ Osenenko <karljosenenko@gmail.com>
2021-04-08 15:44:18 -0600

    created posts reducer

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    posts: []
+}
+
+const postsReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
+export default postsReducer
