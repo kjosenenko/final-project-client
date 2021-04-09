428706581ce3adf3a1564468ea8baebc0f72abff
KJ Osenenko <karljosenenko@gmail.com>
2021-04-08 18:44:52 -0600

    rendered posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 class Post extends Component {
     render() {
+        const {author, post, subject} = this.props
         return (
-            <div>
-                Post
+            <div className="card">
+                <h5 className="card-title">{subject}</h5>
+                <small className="card-text">by: {author}</small>
+                <p className="card-text">{post}</p>
             </div>
         )
     }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import Post from './Post'
 
 class Posts extends Component {
     render() {
+        const posts = this.props.posts.map((post, i) => <Post author={post.author} subject={post.subject} post={post.post} />)
         return (
             <div>
-                Posts
+                {posts}
             </div>
         )
     }
 }
 
-export default Posts
+const mapStateToProps = state => {
+    return {
+        posts: state.posts.posts
+    }
+}
+
+export default connect(mapStateToProps)(Posts)
