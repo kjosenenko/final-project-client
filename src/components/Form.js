c06e431d329c9b24053a81a94c0b2b29337d556e
KJ Osenenko <karljosenenko@gmail.com>
2021-04-10 13:10:25 -0600

    connected form to API for POST action

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {addPost} from '../actions/post'
 
 class Form extends Component {
+    state = {
+        subject: "",
+        author: "",
+        post: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addPost(this.state, this.props.history)
+    }
+
     render() {
         return (
-            <form className="white">
+            <form className="white" onSubmit={this.handleSubmit}>
                 <div className="mb-3 row">
                     <div className="col">
-                        <label>Subject</label><br/>
-                        <input/>
+                        <label htmlFor="subject">Subject</label><br/>
+                        <input type="text" id="subject" name="subject" value={this.state.subject} onChange={this.handleChange}/>
                     </div>
                     <div className="col">
-                        <label>Author</label><br/>
-                        <input/>
+                        <label htmlFor="author">Author</label><br/>
+                        <input type="text" id="author" name="author" value={this.state.author} onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div className="mb-3">
-                    <label>Post</label><br/>
-                    <textarea></textarea>
+                    <label htmlFor="post">Post</label><br/>
+                    <textarea id="post" name="post" value={this.state.post} onChange={this.handleChange}></textarea>
                 </div>
                 <button className="btn btn-dark">Publish Post</button>
             </form>
@@ -24,4 +43,4 @@ class Form extends Component {
     }
 }
 
-export default Form
+export default connect(null, {addPost})(Form)
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -9,6 +9,11 @@ const postsReducer = (state=initialState, action) => {
                 ...state,
                 posts: action.posts
             }
+        case "ADD_POST":
+            return {
+                ...state,
+                posts: [...state.posts, action.post]
+            }
         default:
             return state
     }
